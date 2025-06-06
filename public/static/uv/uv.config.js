a840108e2ec57faa186a9427979786d27b665ceb
321EZ123 <139788167+321EZ123@users.noreply.github.com>
2025-06-06 11:35:10 -0400

    Update uv.config.js

--- public/static/uv/uv.config.js
+++ public/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/insanity/',
-    bare:'https://bare.cbgames.org/',
+    bare:'https://qwertyuiop.24x7.hk/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
