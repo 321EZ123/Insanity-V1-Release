102638a0cf941686ea737d7a594ef9afaba0a1cf
321EZ123 <139788167+321EZ123@users.noreply.github.com>
2025-07-29 15:26:05 -0400

    Update uv.config.js

--- public/static/uv/uv.config.js
+++ public/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/insanity/',
-    bare:'https://focus-bare-nine.vercel.app/',
+    bare:'https://76-76-21-21.ip.addr.tools/', //dont steal my bare instead deploy one yorself on vercel via https://github.com/321EZ123/focus-bare
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
