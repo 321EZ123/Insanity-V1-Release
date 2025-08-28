b8a3ead30d9fa8dcf1c97ecb9d945e899a01c01e
321EZ123 <139788167+321EZ123@users.noreply.github.com>
2025-08-28 17:35:54 -0400

    Update uv.config.js

--- public/static/uv/uv.config.js
+++ public/static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/insanity/',
-    bare:'wss://hyperion-v1.us.to/wisp', //dont steal my bare instead deploy one yorself on vercel via https://github.com/321EZ123/focus-bare
+    bare:'https://hyperion-v1.us.to/bare', //dont steal my bare instead deploy one yorself on vercel via https://github.com/321EZ123/focus-bare
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
